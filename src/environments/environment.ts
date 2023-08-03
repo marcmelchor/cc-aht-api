49fb90666e3283c21e03a784f89e67729c361982
Marc Melchor <93375367+marcmelchor-carcutter@users.noreply.github.com>
2023-08-03 11:58:18 +0200

    feat: Server running.

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  port: 3000,
+  production: false,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import express, { Express } from 'express';
+
+import { environment } from './environments/environment';
+
+const app: Express = express();
+app.use(express.json);
+
+const PORT: number = environment.port;
+
+app.listen(PORT, (): void => {
+  console.log(`Server running on port ${PORT}`);
+});
