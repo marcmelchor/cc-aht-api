0a0cc209b7cfc226aef104e3cf3f2dde91aed115
Marc Melchor <93375367+marcmelchor-carcutter@users.noreply.github.com>
2023-08-07 14:43:53 +0200

    chore: Refactor inges-data endpoint.

--- src/controllers/ingest-data.controller.ts
+++ src/controllers/ingest-data.controller.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from 'express';
-import { AxiosResponse } from 'axios';
 
 import { environment } from '../environments/environment';
 import { transformAndLoad } from '../services/transform-and-load.service';
 
 
 export const ingestData = async (req: Request, res: Response): Promise<Response> => {
-  const response: AxiosResponse = await transformAndLoad(environment.transportAndLoadToken, req.body);
-  // TODO: The body is not really necessary, it was implemented in order to test the integration between micro-services
-  if (response.status === 200) {
-    return res.status(200).send({message: 'OK'}).end();
-  } else {
-    return res.status(400).send({message: 'NOT OK'}).end();
+  try {
+    await transformAndLoad(environment.transportAndLoadToken, req.body);
+    return res.send({message: 'Correctly consumed extract.'}).end();
+  } catch (err: any) {
+    return res.status(400).send({message: `Internal server error extracting: ${err.message}`}).end();
   }
 }
